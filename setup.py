4d5b17a5c258192b58f7c324d1c5265fad8ddc59
Adam Kadlec <adam.kadlec@gmail.com>
2021-12-04 11:44:49 +0100

    Fixing dependencies

--- setup.py
+++ setup.py
@@ -60,11 +60,12 @@ setup(
     package_data={"devices_module": ["py.typed"]},
     install_requires=[
         "fastnumbers",
-        "fastybird-application-exchange",
+        "fastybird-exchange-plugin",
         "fastybird-modules-metadata",
         "kink",
         "pony",
         "setuptools",
+        "mysqlclient",
     ],
     download_url="https://github.com/FastyBird/devices-module/archive/%s.tar.gz" % VERSION,
     classifiers=[

ebc8b9e897a7f880537950b9e7f9571b08c2d865
GitHub <action@github.com>
2023-05-04 17:53:42 +0000

    [bootstrap-library] Refactored logger configuration (#111)

--- tools/phpunit-bootstrap.php
+++ tools/phpunit-bootstrap.php
@@ -6,6 +6,10 @@ if (!defined('FB_TEMP_DIR')) {
 	define('FB_TEMP_DIR', __DIR__ . '/../var/tools/PHPUnit/tmp');
 }
 
+if (!defined('FB_LOGS_DIR')) {
+	define('FB_LOGS_DIR', __DIR__ . '/../var/tools/PHPUnit/logs');
+}
+
 if (@!include __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Tester using `composer update --dev`';
 	exit(1);

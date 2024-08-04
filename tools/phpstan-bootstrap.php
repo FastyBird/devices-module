153316f04293c15458e12a5f048e4a42d7eb2228
GitHub <action@github.com>
2024-08-04 08:22:45 +0000

    [general] Updating phpcs config (#280)

--- tools/phpstan-bootstrap.php
+++ tools/phpstan-bootstrap.php
@@ -1,19 +1,25 @@
 <?php declare(strict_types = 1);
 
+use DG\BypassFinals;
+
 if (!defined('FB_APP_DIR')) {
 	define('FB_APP_DIR', 'val');
 }
+
 if (!defined('FB_RESOURCES_DIR')) {
 	define('FB_RESOURCES_DIR', 'val');
 }
+
 if (!defined('FB_TEMP_DIR')) {
 	define('FB_TEMP_DIR', 'val');
 }
+
 if (!defined('FB_LOGS_DIR')) {
 	define('FB_LOGS_DIR', 'val');
 }
+
 if (!defined('FB_CONFIG_DIR')) {
 	define('FB_CONFIG_DIR', 'val');
 }
 
-DG\BypassFinals::enable();
+BypassFinals::enable();
